39cb626aefe7c9cdb66b98662856c5b841c5f567
maggieliz <maggielizward@gmail.com>
2021-11-22 13:28:48 +0000

    Added functions for token to auth.js and added auth requests to api.js.

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
+import { getToken } from './auth'
 
 const baseURL = '/api'
+
+// * Pattern Requests
 
 export function getAllPatterns() {
   return axios.get(`${baseURL}/patterns`)
@@ -18,3 +21,22 @@ export function addComment(patternID, formdata) {
   return axios.post(`${baseURL}/patterns/${patternID}/comments`, formdata)
 }
 
+// * Auth Requests
+
+export function getHeaders() {
+  return {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  }
+}
+
+export function registerUser(formdata) {
+  return axios.post(`${baseURL}/register`, formdata)
+}
+
+export function loginUser(formdata) {
+  return axios.post(`${baseURL}/login`, formdata)
+}
+
+export function getProfile() {
+  return axios.get(`${baseURL}/profile`, getHeaders())
+}
